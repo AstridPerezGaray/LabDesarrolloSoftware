7bce276223b2e547ac52e68a419155188d9cdad3
AstridPerezGaray <astridperezg3@gmail.com>
2023-09-01 14:09:29 -0600

    Agregando la Guia 1

---
+++ Guia1AstridPerez/src/guia1astridperez/Guia1AstridPerez.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guia1astridperez;
+
+/**
+ *
+ * @author Astrid
+ */
+public class Guia1AstridPerez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        jCalculo F = new jCalculo();
+        F.setVisible(true);
+    }
+    
+}
