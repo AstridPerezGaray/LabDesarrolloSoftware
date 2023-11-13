53625d33ff45a43533d6a4c81c8e38a812192c52
AstridPerezGaray <astridperezg3@gmail.com>
2023-11-12 21:30:15 -0600

    Se van subiendo las vistas y css

---
+++ Vistas/VistaEmpleados.sql
@@ -0,0 +1,26 @@
+CREATE VIEW VistaEmpleados AS
+SELECT 
+    E.ID_Empleado,
+    E.NombresEmpleado,
+    E.ApellidosEmpleado,
+    E.FechaNac,
+    E.Telefono,
+    E.Correo,
+    E.DUI,
+    E.ISSS,
+    D.Linea1,
+    D.linea2,
+    Di.NombreDistrito,
+    M.NombreMunicipio,
+    Dep.NombreDepartamento,
+    T.NombreTipoEmpleado 
+FROM Empleados E
+INNER JOIN Direcciones D ON E.ID_Direccion = D.ID_Direccion
+INNER JOIN Distritos Di ON D.ID_Distrito = Di.ID_Distrito
+INNER JOIN Municipios M ON Di.ID_Municipio = M.ID_Municipio
+INNER JOIN Departamentos Dep ON M.ID_Departamento = Dep.ID_Departamento
+INNER JOIN TipoEmpleado T ON E.ID_TipoEmpleado = T.ID_TipoEmpleado;
+
+
+select * from VistaEmpleados;
+select * from VistaDirecciones
